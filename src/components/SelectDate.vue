8018c0d8f9f3e05c4f87e76fd67510acfa41b412
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-06-09 18:05:11 -0300

    Finalization of test features.

--- src/components/SelectDate.vue
+++ src/components/SelectDate.vue
@@ -2,53 +2,67 @@
   <div class="real-time">
     <form>
       <input
-        type="text"
+        type="date"
         v-model="date"
-        placeholder="Write a date: (DD-MM-YYYY)"
+        pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}"
+        required
       />
+      <select v-model="coin">
+        <option value="bitcoin">BITCOIN</option>
+        <option value="dacxi">DACXI</option>
+        <option value="ethereum">ETH</option>
+        <option value="cosmos">ATOM</option>
+        <option value="terra-luna">LUNA</option>
+      </select>
       <select v-model="base_input">
-        <option value="USD">USD</option>
-        <option value="XAU">XAU</option>
-        <option value="BRL">BRL</option>
-        <option value="EUR">EUR</option>
+        <option value="usd">USD</option>
+        <option value="xau">XAU</option>
+        <option value="brl">BRL</option>
+        <option value="eur">EUR</option>
       </select>
       <input type="submit" value="Show value" v-on:click="submitForm" />
     </form>
-    <p id="answer">{{ base }} {{ value }}</p>
+    <p id="answer" v-if="toggle">{{ base }} {{ value }}</p>
+    <p id="answer" v-else>Sorry, invalid date!</p>
   </div>
 </template>
 
 <script>
 import { anyTime } from "../requests";
-import { realTimeRequest } from "../requests";
 export default {
   name: "SelectDate",
   data: () => {
     return {
       coin: "bitcoin",
       date: "",
-      base_input: "USD",
-      base: "USD",
+      date_value: "",
+      base_input: "usd",
+      base: "",
       value: "",
+      toggle: true,
     };
   },
   methods: {
-    async changeValue(date, base) {
-      this.value = await anyTime(date, base);
+    async changeValue(coin, date, base) {
+      this.value = await anyTime(coin, date, base);
+      if (this.value === undefined) {
+        this.toggle = false;
+      } else {
+        this.toggle = true;
+      }
+    },
+    changeDateValue() {
+      const day = this.date.slice(-2);
+      const month = this.date.slice(5, 7);
+      const year = this.date.slice(0, 4);
+      this.date_value = `${day}-${month}-${year}`;
     },
     submitForm(e) {
       e.preventDefault();
-      this.base = this.base_input;
-      const lower = this.base.toLowerCase();
-      this.changeValue(this.date, lower);
-      this.date = "";
+      this.changeDateValue();
+      this.base = `${this.date}: ${this.base_input}`;
+      this.changeValue(this.coin, this.date_value, this.base_input);
     },
-    async firtsRequest() {
-      this.value = await realTimeRequest();
-    },
-  },
-  created() {
-    this.firtsRequest();
   },
 };
 </script>
--- src/requests.js
+++ src/requests.js
@@ -6,10 +6,13 @@ export const realTimeRequest = async () => {
   return value;
 };
 
-export const anyTime = async (date, base) => {
-  const baseUrl = `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=${date}`;
+export const anyTime = async (coin, date, base) => {
+  const baseUrl = `https://api.coingecko.com/api/v3/coins/${coin}/history?date=${date}`;
   const result = await fetch(baseUrl);
   const data = await result.json();
+  if (data.error === "invalid date") {
+    return undefined;
+  }
   const value = data["market_data"]["current_price"][base].toFixed(2);
   return value;
 };
